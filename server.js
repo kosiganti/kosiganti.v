4de89785d60c71557cc69770637b620809e18d0b
kosiganti.v <kosiganti.v@husky.neu.edu>
2017-04-06 02:55:48 -0400

    server

--- server.js
+++ server.js
@@ -23,9 +23,6 @@ app.set('view engine', 'ejs');
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-require ("./test/app.js")(app);
-require ("./assignment/app.js")(app);
-
 var port = process.env.PORT || 3000;
 
 app.listen(port);
